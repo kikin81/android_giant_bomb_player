ff6907078d7737d83be43a19dcbdd4f2fcbcae30
Francisco Velazquez <kikin81@gmail.com>
2020-11-22 15:31:30 -0800

    Adds published date to model and view (#10)
    Adds published date to model and view
    
    Resolves #9
    
    Co-authored-by: Francisco Velazquez <francisco.velazquez@avast.com>

--- app/src/main/java/us/kikin/apps/android/bgplayer/models/VideoModel.kt
+++ app/src/main/java/us/kikin/apps/android/bgplayer/models/VideoModel.kt
@@ -1,9 +1,45 @@
 package us.kikin.apps.android.bgplayer.models
 
+import android.text.format.DateUtils
+import java.util.Date
+import us.kikin.apps.android.bgplayer.network.VideoDto
+
 data class VideoModel(
     val id: Long,
     val name: String,
-    val length: Int,
+    val length: Long,
     val thumbnailUrl: String,
-    val description: String
-)
+    val description: String,
+    val publishedDate: Date
+) {
+    constructor(dto: VideoDto) :
+        this(
+            dto.id,
+            dto.name,
+            dto.length,
+            dto.imageDto.screenUrl,
+            dto.description,
+            dto.publishedDate
+        )
+
+    val publishedRelativeDay: String by lazy {
+        val now = System.currentTimeMillis()
+        DateUtils.getRelativeTimeSpanString(
+            publishedDate.time,
+            now,
+            DateUtils.MINUTE_IN_MILLIS
+        ).toString()
+    }
+
+    val runtimeDisplay: String by lazy {
+        val hours = length / 3600
+        val minutes = (length % 3600) / 60
+        val seconds = length % 60
+
+        if (hours > 0) {
+            String.format("%d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format("%02d:%02d", minutes, seconds)
+        }
+    }
+}
--- app/src/main/java/us/kikin/apps/android/bgplayer/repository/VideoRepository.kt
+++ app/src/main/java/us/kikin/apps/android/bgplayer/repository/VideoRepository.kt
@@ -10,24 +10,10 @@ class VideoRepository @Inject constructor(
     private val videoApiHelper: VideoApiHelper
 ) {
     suspend fun getVideos() =
-        videoApiHelper.getVideos().videos.map {
-            VideoModel(
-                it.id,
-                it.name,
-                it.length,
-                it.imageDto.screenUrl,
-                it.description
-            )
-        }
+        videoApiHelper.getVideos().videos.map { VideoModel(it) }
 
     suspend fun getVideoById(videoId: Long): VideoModel {
         val response = videoApiHelper.getVideoById(videoId).video
-        return VideoModel(
-            response.id,
-            response.name,
-            response.length,
-            response.imageDto.screenUrl,
-            response.description
-        )
+        return VideoModel(response)
     }
 }
