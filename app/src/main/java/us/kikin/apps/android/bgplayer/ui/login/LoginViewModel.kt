25f1021eb786b5a6da9a60f3bb5092c88488ee3e
Francisco Velazquez <kikin81@gmail.com>
2020-11-28 13:20:50 -0800

    Implement sign in webview (#36)
    * Implement sign in webview
    
    Added webview fragment to load sign in url
    Need to auth with the code
    
    Resolves #35

---
+++ app/src/main/java/us/kikin/apps/android/bgplayer/ui/login/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package us.kikin.apps.android.bgplayer.ui.login
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel @ViewModelInject constructor() : ViewModel()
