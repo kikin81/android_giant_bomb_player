88008c7955b9015174bf6f05e32fce0e5e96777f
Francisco Velazquez <kikin81@gmail.com>
2020-11-27 23:40:02 -0800

    Implement sign in webview
    Added webview fragment to load sign in url
    Need to auth with the code
    
    Resolves #35

---
+++ app/src/main/java/us/kikin/apps/android/bgplayer/ui/login/LoginFragment.kt
@@ -0,0 +1,56 @@
+package us.kikin.apps.android.bgplayer.ui.login
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import us.kikin.apps.android.bgplayer.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = requireNotNull(_binding)
+    private val viewModel: LoginViewModel by viewModels()
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        with(binding) {
+            webview.settings.javaScriptEnabled = true
+            webview.webViewClient = LoginWebViewClient()
+            webview.loadUrl(LOGIN_URL)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private class LoginWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            if (Uri.parse(url).host == "www.giantbomb.com") {
+                // Giant bomb site, do not override
+                return false
+            }
+            return true
+        }
+    }
+
+    companion object {
+        private const val LOGIN_URL = "https://www.giantbomb.com/app/bgplayer/"
+    }
+}
